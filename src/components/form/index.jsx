a03eee62accdccee4c84ab7907d9dba0665dec5d
Restu Averian <restuaverianputra123@gmail.com>
2026-05-16 18:11:01 +0700

    feat: update tampilan mobile using useBreakpoint

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -9,17 +9,19 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import BookForm from "./book-form";
+import { useBreakpoint } from "@/js-toolkit/src/react";
 
 export default function FormBookDrawer() {
+  const { xs } = useBreakpoint();
   return (
-    <Drawer direction="right">
+    <Drawer direction={xs ? "bottom" : "right"}>
       <DrawerTrigger asChild>
         <Button
-          className="h-9 rounded-md bg-primary-accent px-4 text-xs font-semibold normal-case tracking-normal text-white shadow-inset-button hover:bg-hover-accent"
+          className="h-auto md:h-9 fixed bottom-5 right-5 md:static rounded-full md:rounded-md bg-primary-accent px-5 py-5 md:py-2 md:px-4 text-xs font-semibold normal-case tracking-normal text-white shadow-inset-button hover:bg-hover-accent "
           type="button"
         >
-          <Plus className="size-4" />
-          Tambah Buku
+          <Plus className="size-6 md:size-4" />
+          {!xs && "Tambah Buku"}
         </Button>
       </DrawerTrigger>
 
